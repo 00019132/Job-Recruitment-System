01be3a9d47ec41f69de3deae91957e98f1f6ba1b
marcusdevel <zenfix014@gmail.com>
2025-12-05 19:24:11 +0500

    commmm

--- Job_Recruitment_System/settings.py
+++ Job_Recruitment_System/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s&^z$43_br2a6hzzf#k!^koa_h58%zk(pry=23^g5v-=i1_mq8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'your-app.onrender.com').split()
 
 
 # Application definition
